bc1291a2c13badde8d9720ff09eae9058d69cd04
npetrukhin <nikolay.petruhin@osinit.com>
2018-10-20 21:28:39 +0300

    проигрываем музыку при старте игры

--- webgame/src/App.js
+++ webgame/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Howl} from 'howler';
 import ChoiceGroup from './containers/ChoiceGroup';
 import MainScreen from './containers/MainScreen';
 import { connect } from "react-redux";
@@ -12,6 +13,15 @@ class App extends Component {
           {group ? <MainScreen/> : <ChoiceGroup/>}
       </div>
     );
+  }
+
+  componentDidMount() {
+    const sound = new Howl({
+      src: ['assets/sounds/group1.mp3']
+    });
+
+    sound.fade(0.1, 0.5, 4000);
+    sound.play();
   }
 }
 
