39341678995859ede368fe396e51fc80dcfce399
totaki <karagabul@gmail.com>
2018-10-20 19:41:03 +0300

    add build

---
+++ docs/precache-manifest.4d35bde2a672d86546d02b238ea97cfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1c7596da74759f83993c",
+    "url": "/static/js/main.1c7596da.chunk.js"
+  },
+  {
+    "revision": "22fe96ab9d3504e088a2",
+    "url": "/static/js/1.22fe96ab.chunk.js"
+  },
+  {
+    "revision": "1c7596da74759f83993c",
+    "url": "/static/css/main.e55cd575.chunk.css"
+  },
+  {
+    "revision": "374eaa1491f40f60f516d55b22f0cd66",
+    "url": "/index.html"
+  }
+];
