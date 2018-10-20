1216829508214f5f485f38d2318325e0115e81a7
npetrukhin <nikolay.petruhin@osinit.com>
2018-10-20 23:51:50 +0300

    модальное окно с информацией о группе

--- webgame/src/App.js
+++ webgame/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import { Howl } from 'howler';
+import React, {Component} from 'react';
+import {Howl} from 'howler';
 import ChoiceGroup from './containers/ChoiceGroup';
 import MainScreen from './containers/MainScreen';
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 import './App.css';
 import Rodal from 'rodal';
 import 'rodal/lib/rodal.css';
 import EventInfo from "./containers/EventInfo";
-import { eventAction } from "./controller";
+import {eventAction, showGroupInfo} from "./controller";
 
 const COMMON_MODAL_STYLE = {
     backgroundColor: "#161616",
@@ -17,30 +17,38 @@ const COMMON_MODAL_STYLE = {
 
 class App extends Component {
 
-  render() {
-    const { group, showEventId, closeEvent, showGroupInfo } = this.props;
-    return (
-      <div className="App">
-          {group ? <MainScreen/> : <ChoiceGroup/>}
-          <Rodal visible={!!showEventId}
-                 onClose={closeEvent}
-                 animation="slideRight"
-                 showCloseButton={false}
-                 customStyles={COMMON_MODAL_STYLE}>
-              Hello world
-              <EventInfo/>
-          </Rodal>
-      </div>
-    );
-  }
+    render() {
+        const {group, showEventId, closeEvent, showGroupInfo, hideGroupInfo} = this.props;
+        return (
+            <div className="App">
+                {group ? <MainScreen/> : <ChoiceGroup/>}
+                <Rodal visible={!!showEventId}
+                       onClose={closeEvent}
+                       animation="slideRight"
+                       showCloseButton={false}
+                       customStyles={COMMON_MODAL_STYLE}>
+                    Hello world
+                    <EventInfo/>
+                </Rodal>
+                <Rodal visible={!!showGroupInfo}
+                       onClose={hideGroupInfo}
+                       animation="slideRight"
+                       showCloseButton={false}
+                       customStyles={COMMON_MODAL_STYLE}>
+                    Информация о группе
+                    <EventInfo/>
+                </Rodal>
+            </div>
+        );
+    }
 
-  componentDidMount() {
-    const sound = new Howl({
-      src: ['assets/sounds/group1.mp3']
-    });
-    sound.fade(0.1, 0.5, 4000);
-    sound.play();
-  }
+    componentDidMount() {
+        const sound = new Howl({
+            src: ['assets/sounds/group1.mp3']
+        });
+        sound.fade(0.1, 0.5, 4000);
+        sound.play();
+    }
 }
 
 function mapStateToProps(state) {
@@ -54,6 +62,7 @@ function mapStateToProps(state) {
 function mapStateToDispatch(dispatch) {
     return {
         closeEvent: () => dispatch(eventAction.show(null)),
+        hideGroupInfo: () => dispatch(showGroupInfo(false))
     };
 }
 
