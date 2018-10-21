5e83b8286b4ed1f9ce101d310b735a133b4131cc
totaki <karagabul@gmail.com>
2018-10-21 19:33:32 +0300

    dollars

---
+++ docs/precache-manifest.ca9e0d83b047e5ea97f0a8d56d5db0f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "715e9b8fb6db20bcd7dd",
+    "url": "/static/js/main.715e9b8f.chunk.js"
+  },
+  {
+    "revision": "502ce489b08c7898ef9f",
+    "url": "/static/js/1.502ce489.chunk.js"
+  },
+  {
+    "revision": "715e9b8fb6db20bcd7dd",
+    "url": "/static/css/main.0607ed38.chunk.css"
+  },
+  {
+    "revision": "502ce489b08c7898ef9f",
+    "url": "/static/css/1.27d45328.chunk.css"
+  },
+  {
+    "revision": "08cda9cfc5773ad50159a650b9f674de",
+    "url": "/index.html"
+  }
+];
